ba8b2acaa51728e520ba00c3f29e295c7daa6adb
ajinakaya <ajina.kayastha5@gmail.com>
2024-12-05 18:16:51 +0545

    changes

--- lib/app.dart
+++ lib/app.dart
@@ -1,7 +1,5 @@
 
 import 'package:flutter/material.dart';
-import 'package:flutter_app/view/arithmetic_view.dart';
-import 'package:flutter_app/view/first_view.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -10,7 +8,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: ArithmeticView(),
+
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/cupertino.dart';
-import 'package:flutter_app/app.dart';
+import 'package:learnara/app.dart';
 import 'package:flutter/material.dart';
 
 
