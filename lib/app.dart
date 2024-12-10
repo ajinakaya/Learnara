935e492d4cf7bc2b181ffd5410cd93a407ad642c
ajinakaya <ajina.kayastha5@gmail.com>
2024-12-10 20:07:29 +0545

    register

--- lib/app.dart
+++ lib/app.dart
@@ -1,5 +1,6 @@
 
 import 'package:flutter/material.dart';
+import 'package:learnara/screen/register_screen.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -8,6 +9,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
+      home: RegisterScreen(),
 
     );
   }
