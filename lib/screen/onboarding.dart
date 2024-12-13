3d2760cb320a148ffe12ed064a8a9577388e26c7
ajinakaya <ajina.kayastha5@gmail.com>
2024-12-13 12:07:09 +0545

    onBoarding page

--- lib/screen/onboarding.dart
+++ lib/screen/onboarding.dart
@@ -1,11 +1,48 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class Onboarding extends StatelessWidget {
+class Onboarding extends StatefulWidget {
   const Onboarding({super.key});
 
   @override
+  State<Onboarding> createState() => _OnboardingState();
+}
+
+class _OnboardingState extends State<Onboarding> {
+  int _currentPage = 0;
+
+  final List<Map<String , String>>_OnboardingData =[
+    {
+      "title": "Learn a new language",
+      "subtitle": "Discover the joy of learning with interactive lessons and practice.",
+      "image": "assets/images/onboarding1.jpg",
+    },
+    {
+      "title": "Track your progress",
+      "subtitle": "Keep an eye on your achievements and reach your goals faster.",
+      "image": "assets/images/progress1.jpg",
+    },
+    {
+      "title": "Select a language",
+      "subtitle": "Choose your preferred language and begin your journey.",
+      "image": "assets/images/onboarding.jpg",
+    },
+  ];
+
+  void _nextPage(){
+    if (_currentPage < _OnboardingData.length-1){
+      setState(() {
+        _currentPage++;
+      });
+    }else{
+      Navigator.of(context).pushNamed("/login");
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final data = _OnboardingData[_currentPage];
+
     return Scaffold(
       backgroundColor: Colors.white,
       body: Padding(
@@ -15,7 +52,7 @@ class Onboarding extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             Text(
-              "Let's learn with us",
+              data["title"]!,
               textAlign: TextAlign.center,
               style: TextStyle(
                 fontSize: 30,
@@ -24,10 +61,9 @@ class Onboarding extends StatelessWidget {
               ),
             ),
             const SizedBox(height: 16),
-
             // Subtitle
             Text(
-              "Set up your language learning journey",
+              data["subtitle"]!,
               textAlign: TextAlign.center,
               style: TextStyle(
                 fontSize: 16,
@@ -35,49 +71,42 @@ class Onboarding extends StatelessWidget {
               ),
             ),
             const SizedBox(height: 40),
-
             Image.asset(
-              'assets/images/onboarding1.jpg',
-              height: 330,
+              data["image"]!,
+              height: 300,
               fit: BoxFit.contain,
             ),
-            const SizedBox(height: 60),
-
-            // Sign-in Button
-            SizedBox(
-              width: double.infinity,
-              child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  padding: const EdgeInsets.symmetric(vertical: 16),
-                  backgroundColor: Colors.black.withOpacity(0.75),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(12),
+            const SizedBox(height: 40),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children:List.generate(
+                _OnboardingData.length,
+                    (index) => Container(
+                  margin: const EdgeInsets.symmetric(horizontal: 4.0),
+                  width: 8,
+                  height: 8,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: _currentPage == index ? Colors.black : Colors.grey,
                   ),
                 ),
-                onPressed: () {
-                  Navigator.of(context).pushNamed("/login");
-                },
-                child: const Text(
-                  "Sign in",
-                  style: TextStyle(fontSize: 18, color: Colors.white),
-                ),
               ),
             ),
-            const SizedBox(height: 16),
-            GestureDetector(
-              onTap: () {
-                Navigator.of(context).pushNamed("/register");
-              },
-              child: Text(
-                "Create an account",
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 16,
-                  color: Colors.blue,
-                  fontWeight: FontWeight.w600,
+            const SizedBox(height:55),
+            ElevatedButton(
+                onPressed: _nextPage,
+                style: ElevatedButton.styleFrom(
+                  padding: const EdgeInsets.symmetric(vertical: 16,horizontal:32),
+                  backgroundColor: Colors.black.withOpacity(0.75),
+                  shape:RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  minimumSize: Size(double.infinity,20),
                 ),
-              ),
-            ),
+                child: Text(
+                  _currentPage < _OnboardingData.length -1 ? "Next": "Get Started",
+                  style: TextStyle(fontSize: 18,color: Colors.white),
+                ))
           ],
         ),
       ),
