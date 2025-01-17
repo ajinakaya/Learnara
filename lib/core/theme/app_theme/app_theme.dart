3d4be9a5b402cacf605e6a637aa6180befe910d4
ajinakaya <ajina.kayastha5@gmail.com>
2025-01-17 14:17:00 +0545

    core

--- lib/core/theme/app_theme/app_theme.dart
+++ lib/core/theme/app_theme/app_theme.dart
@@ -1,9 +1,89 @@
 import 'package:flutter/material.dart';
+import 'package:learnara/app/constants/theme_constant.dart';
 
-ThemeData getApplicationTheme(){
-  return  ThemeData(
-    scaffoldBackgroundColor: Colors.white,
-    fontFamily:'Poppins-Regular',
+class AppTheme {
+  AppTheme._();
 
-  );
+  static ThemeData getApplicationTheme() {
+    return ThemeData(
+      // Define light mode color scheme
+      colorScheme: const ColorScheme.light(
+        primary: ThemeConstant.primaryColor,
+      ),
+      brightness: Brightness.light,
+      fontFamily: 'Poppins-Regular',
+      useMaterial3: true,
+
+      // App bar theme
+      appBarTheme: const AppBarTheme(
+        elevation: 0,
+        backgroundColor: ThemeConstant.appBarColor,
+        centerTitle: true,
+        titleTextStyle: TextStyle(
+          color: Colors.black,
+          fontSize: 20,
+        ),
+      ),
+
+      // Elevated button theme
+      elevatedButtonTheme: ElevatedButtonThemeData(
+        style: ElevatedButton.styleFrom(
+          elevation: 0,
+          foregroundColor: Colors.white,
+          backgroundColor: ThemeConstant.primaryColor,
+          padding: const EdgeInsets.symmetric(vertical: 16),
+          textStyle: const TextStyle(
+            fontSize: 20,
+          ),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(12),
+          ),
+        ),
+      ),
+
+      // Text field theme
+      inputDecorationTheme: InputDecorationTheme(
+        contentPadding: const EdgeInsets.all(15),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10),
+        ),
+        labelStyle: const TextStyle(
+          fontSize: 16,
+          color: Colors.black,
+        ),
+        hintStyle: const TextStyle(
+          fontSize: 16,
+          color: Colors.grey,
+        ),
+        errorBorder: const OutlineInputBorder(
+          borderSide: BorderSide(
+            color: Colors.red,
+          ),
+        ),
+        enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(14),
+          borderSide: BorderSide(color: Colors.grey, width: 1),
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(14),
+          borderSide: BorderSide(color: Colors.grey, width: 1),
+        ),
+        prefixIconColor: ThemeConstant.primaryColor,
+        suffixIconColor: ThemeConstant.primaryColor,
+      ),
+      // Circular progress indicator theme
+      progressIndicatorTheme: const ProgressIndicatorThemeData(
+        color: ThemeConstant.primaryColor,
+      ),
+
+      // Bottom navigation bar theme
+      bottomNavigationBarTheme: const BottomNavigationBarThemeData(
+        backgroundColor: Colors.white,
+        selectedItemColor: Colors.black,
+        unselectedItemColor: Colors.grey,
+        type: BottomNavigationBarType.fixed,
+        elevation: 0,
+      ),
+    );
+  }
 }
