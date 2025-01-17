d0bbfb1cb27f9d3a343449bf8148de5d3d9a009b
ajinakaya <ajina.kayastha5@gmail.com>
2025-01-17 14:25:10 +0545

    login view_model

--- lib/features/auth/presentation/view_model/login/login_bloc.dart
+++ lib/features/auth/presentation/view_model/login/login_bloc.dart
@@ -1,11 +1,9 @@
 import 'package:equatable/equatable.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:softwarica_student_management_bloc/app/di/di.dart';
-import 'package:softwarica_student_management_bloc/features/auth/presentation/view_model/signup/register_bloc.dart';
-import 'package:softwarica_student_management_bloc/features/batch/presentation/view_model/batch_bloc.dart';
-import 'package:softwarica_student_management_bloc/features/course/presentation/view_model/course_bloc.dart';
-import 'package:softwarica_student_management_bloc/features/home/presentation/view_model/home_cubit.dart';
+import 'package:learnara/features/auth/presentation/view_model/signup/register_bloc.dart';
+import 'package:learnara/features/home/presentation/view_model/home_cubit.dart';
+
 
 part 'login_event.dart';
 part 'login_state.dart';
@@ -26,8 +24,6 @@ class LoginBloc extends Bloc<LoginEvent, LoginState> {
           MaterialPageRoute(
             builder: (context) => MultiBlocProvider(
               providers: [
-                BlocProvider.value(value: getIt<CourseBloc>()),
-                BlocProvider.value(value: getIt<BatchBloc>()),
                 BlocProvider.value(value: _registerBloc),
               ],
               child: event.destination,
