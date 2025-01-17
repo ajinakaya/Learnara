a8c57fc9201dc589ce443e8715358dffec469f2c
ajinakaya <ajina.kayastha5@gmail.com>
2025-01-17 14:26:15 +0545

    register view_model

--- lib/features/auth/presentation/view_model/signup/register_bloc.dart
+++ lib/features/auth/presentation/view_model/signup/register_bloc.dart
@@ -1,8 +1,8 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flutter/material.dart';
-import 'package:learnara/core/common/snackbar/my_snackbar.dart';
-import 'package:learnara/features/auth/domain/use_case/register_user_usecase.dart';
+import 'package:learnara/core/common/snack_bar/my_snackbar.dart';
+import 'package:learnara/features/auth/domain/usecase/register_user_usecase.dart';
 
 
 part 'register_event.dart';
@@ -12,14 +12,11 @@ class RegisterBloc extends Bloc<RegisterEvent, RegisterState> {
   final RegisterUseCase _registerUseCase;
 
   RegisterBloc({
-
     required RegisterUseCase registerUseCase,
-  })  :
-        _registerUseCase = registerUseCase,
+  })  : _registerUseCase = registerUseCase,
         super(RegisterState.initial()) {
     on<RegisterUser>(_onRegisterEvent);
   }
-
 
   void _onRegisterEvent(
       RegisterUser event,
